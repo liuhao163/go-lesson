f8cc95be06f75b02fee81c6d428a17f10404c51d
liuhao163 <liuhaoeric@didichuxing.com>
2018-01-25 22:42:35 +0800

    add stdin

--- channel/CommonChannel2.go
+++ channel/CommonChannel2.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 var a1 string
-var c1 = make(chan int)
+var c1 = make(chan string)
 
 func f1() {
 	a1 = "hello, world"
-	<-c1
+	c1 <- a1
 }
 
-func main(){
+func main() {
 	go f1();
-	c1<-0
-	fmt.Print(a1)
+	a2 := <-c1
+	fmt.Print(a2)
 }
